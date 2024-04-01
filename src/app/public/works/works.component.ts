b857530265cb5688ae2370c6a19592fc24d1c11e
umarllee <gulchohreum20631@sabah.edu.az>
2024-04-01 17:28:07 +0400

    ui

--- src/app/public/works/works.component.ts
+++ src/app/public/works/works.component.ts
@@ -10,34 +10,74 @@ export class WorksComponent implements OnInit {
     {
       url: '../../../assets/projects/table.webp',
       color: 'cornflowerblue',
-      title: 'Multifunctional table',
-      link: '../../tables'
+      title: 'Multifunctional table', 
+      link: '../../tables',
+      hiperLink: ''
     },
     {
       url: '../../../assets/projects/cards.webp',
       color: 'darkorange',
       title: 'Cards ui',
-      link: '../../cards' 
+      link: '../../cards',
+      hiperLink: ''
     },
     {
       url: '../../../assets/projects/illdy.png',
       color: '#9fcb42',
       title: 'Illdy prototype',
-      link: '../../illdy'
-    },
-    {
-      url: '../../../assets/projects/layout.png',
-      color: '#8d8ae6',
-      title: 'Figma layouts',
-      link: '../../layouts'
+      link: '../../illdy',
+      hiperLink: ''
     },
     {
       url: '../../../assets/projects/draggable.png',
       color: 'rgb(250 208 44)',
       title: 'Draggable elements',
-      link: '../../draggable'
+      link: '../../draggable',
+      hiperLink: ''
     },
 
+    {
+      url: '../../../assets/projects/dazko.svg',
+      color: '#4fabf7',
+      title: 'Dazko',
+      link: '',
+      hiperLink: 'https://inquisitive-yeot-fd19d4.netlify.app/'
+    },
+    {
+      url: '../../../assets/projects/layout.png',
+      color: '#8d8ae6',
+      title: '"Illdy" layout',
+      link: '',
+      hiperLink: 'https://thriving-youtiao-4b75d8.netlify.app/'
+    },
+    {
+      url: '../../../assets/projects/watchFilm.png',
+      color: '#e2905a',
+      title: 'How can watch film?',
+      link: '',
+      hiperLink: 'https://illustrious-pie-12d75a.netlify.app/'
+    },
+    {
+      url: '../../../assets/projects/seramic.png',
+      color: '#dfa5a8',
+      title: 'Mina Seramic (React)',
+      link: '',
+      hiperLink: 'https://celadon-pika-fd5113.netlify.app/'
+    },
+    {
+      url: '../../../assets/projects/car.png',
+      color: '#df421c',
+      title: 'Car (React)',
+      link: '',
+      hiperLink: 'https://illustrious-dusk-629bac.netlify.app/'
+    },
+    {
+      url: '../../../assets/projects/trello.png',
+      color: '#4643d2',
+      title: 'Mini trello (Angular)',
+      link: '',
+      hiperLink: 'https://sensational-maamoul-6416ce.netlify.app/main/task'
+    },
   ];
 
   constructor(){
