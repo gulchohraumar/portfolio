b8aa4eb158fc271c06c4c818f91f936e98a09051
umarllee <gulchohreum20631@sabah.edu.az>
2024-04-01 08:50:34 +0400

    ui

--- src/app/public/about/about.component.ts
+++ src/app/public/about/about.component.ts
@@ -7,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  skillsList = [
+    {
+      name: 'Html5'
+    },
+    {
+      name: 'Css3'
+    },
+    {
+      name: 'Bootstrap'
+    },
+    {
+      name: 'Tailwind'
+    },
+    {
+      name: 'Material UI'
+    },
+    {
+      name: 'JavaScript'
+    },
+    {
+      name: 'TypeScript'
+    },
+    {
+      name: 'Angular'
+    },
+    {
+      name: 'React (Intermediate)'
+    },
+    {
+      name: 'RxJS'
+    },
+    {
+      name: 'Context API'
+    },
+    {
+      name: 'Basic Redux'
+    },
+    {
+      name: 'RestFull API'
+    },
+    {
+      name: 'Git & Github'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
